ace62cfedfd545e74ce1379b2085f2ce1d4f720a
Junaid <jasaanu123@gmail.com>
2026-01-31 11:08:46 +0530

    Time: 9 ms (6.23%) | Memory: 53.2 MB (97.66%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function(arr) {
+    let count = 0
+    for(i=0;i<arr.length;i++){
+        for(j=0;j<arr.length;j++){
+            if(arr[i] == 2 * arr[j] && i != j)count++
+        }
+    }
+    return count ? true:false
+};
